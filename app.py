c5bedb27553750e36b3422dd59b50e0152ad2931
arslanebouali <boualiarslane@gmail.com>
2022-07-28 14:30:59 +0200

    memory usage improvement

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from Utils.predicter import PipelinePredictor
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template,send_file,after_this_request
 from Utils.records import data
 from Utils.Tools import *
+import os
 import pickle
 
 full_model=('models/lgbm_full_model.sav')
@@ -21,6 +22,7 @@ predictor = PipelinePredictor(model_path=model, explainer_path=explainer)
 def home():
     return "Home"
 
+
 Data= data()
 @app.route('/predict/<id_client>', methods=['GET'])
 def predict(id_client):
@@ -30,18 +32,18 @@ def predict(id_client):
 
     prediction,proba = predictor.predict(client_full_predict)
 
-
+    del client_full ,client_full_predict
 
 
     dict={"proba": proba.tolist(),
           "prediction " : prediction.tolist()}
-
+    del prediction,proba
     resp = jsonify(dict)
     resp.status_code = 200
     print(resp)
     return resp
 
-from flask import send_file
+
 
 @app.route('/explain/<id_client>')
 def get_image(id_client):
@@ -50,6 +52,19 @@ def get_image(id_client):
     exp=predictor.explanation(client_full)
     predictor.explain(exp)
     filename= 'data/explanation.txt'
+    del client_full, client_full_predict,exp
+    @after_this_request
+    def remove_file(response):
+        file_handle = open(filename, 'r')
+        try:
+            os.remove(filename)
+            file_handle.close()
+            del file_handle
+            print("Txt file deleted")
+        except Exception as error:
+            app.logger.error("Error removing or closing downloaded file handle", error)
+        return response
+
     return send_file(filename, mimetype='html')
 
 @app.route('/df_maps/<id_client>', methods=['GET'])
@@ -66,7 +81,7 @@ def get_maps(id_client):
             "filtered_neg": df_map_filtered_neg.to_json()}
 
     resp = jsonify(dict)
-
+    del client_full, client_full_predict,dict,df_map_filtered_neg,df_map_filtered_plus,exp
     print(resp)
     return resp
 
